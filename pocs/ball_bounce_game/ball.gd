69cc2a39e7180f10cbb40db0d9b11b7b5323511c
conor-wilson <conorwilson1998@gmail.com>
2025-11-08 13:37:34 +1100

    Removed POCs

--- pocs/ball_bounce_game/ball.gd
+++
@@ -1,1 +0,0 @@
-class_name Ball extends RigidBody2D
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,7 +1,4 @@
 extends Node2D
 
-@export var mexican_wave_typing_game_scene:PackedScene
-
-
 func _on_endless_runner_button_pressed() -> void:
 	SceneSwitcher.queue_switch_scene(SceneSwitcher.endless_runner_game)
