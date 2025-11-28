71a06dbfbf0e8e7846954c9333268ec3c75a37f3
Clicksurfer <amit.chameides@gmail.com>
2025-11-28 19:10:17 +1100

    Added tutorial to lobby

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 @export var _play_button:Button
 @export var _settings_button:Button
+@export var _tutorial_button:Button
 
 @export var _settings_popup:PauseMenu
+@export var _tutorial_popup:TutorialPopup
 
 func _ready() -> void:
 	_setup_buttons()
@@ -13,10 +15,15 @@ func _setup_buttons() -> void:
 		_play_button.pressed.connect(_on_play_button_pressed)
 	if !_settings_button.pressed.is_connected(_on_settings_button_pressed):
 		_settings_button.pressed.connect(_on_settings_button_pressed)
+	if !_tutorial_button.pressed.is_connected(_on_tutorial_button_pressed):
+		_tutorial_button.pressed.connect(_on_tutorial_button_pressed)
 
 func _on_play_button_pressed() -> void:
 	SceneSwitcher.queue_switch_scene(SceneSwitcher.endless_runner_game)
 
 func _on_settings_button_pressed() -> void:
 	_settings_popup.open_popup()
+
+func _on_tutorial_button_pressed() -> void:
+	_tutorial_popup.show()
 	
